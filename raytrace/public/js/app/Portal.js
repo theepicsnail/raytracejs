05d744e9f9e09372a9065a56122726314b08f116
Steven <theepicsnail@gmail.com>
2014-06-22 18:41:34 -0400

    Reworked portal logic to support more gneeral position/directions

--- raytrace/public/js/app/Portal.js
+++ raytrace/public/js/app/Portal.js
@@ -4,10 +4,12 @@ define([], function() {
   function Portal() {
   }
 
-  Portal.prototype.setEntrance = function(row, col, dir) {
+  Portal.prototype.setEntrance = function(x, y, dir) {
+
   };
 
-  Portal.prototype.setExit = function(row, col, dir) {
+  Portal.prototype.setExit = function(x, y, dir) {
+
   };
 
   Portal.prototype.rayEnters = function(x, y, dx, dy) {
@@ -15,11 +17,23 @@ define([], function() {
   };
 
   Portal.prototype.cross = function(x, y, dir, dist) {
+    var delta_angle = 0;// +new Date()/2000;
+    var cos = Math.cos(delta_angle), sin = Math.sin(delta_angle);
+    var offset_x = (x%1)-0.5, offset_y = (y%1)-0.5;
+    var newX = 6 +  offset_x * cos + offset_y * -sin + 0.5;
+    var newY = 7 +  offset_x * sin + offset_y * cos  + 0.5;
+    ///newX += Math.sin( + new Date()/2000)-1;
     return {
-      x: 3 + (1 - y%1),
-      y: 3 + (    x%1),
-      dir: dir + Math.PI/2
+      x: newX,
+      y: newY,
+      dir: dir + delta_angle
     };
+
+    /*return {
+      x: 6 + x%1,//3 + (1 - y%1),
+      y: 7 + y%1,//3 + (    x%1),
+      dir: dir// + Math.PI/2
+    };*/
     // Project the movement through the portal and
     // return the resulting x, y, dx, dy
   };
