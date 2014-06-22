c2d8fb627e5d8d4c0df46dcf422f911232c9f78a
Steven <theepicsnail@gmail.com>
2014-06-21 20:28:24 -0400

    Limited fps to 30

--- raytrace/public/js/app/GameLoop.js
+++ raytrace/public/js/app/GameLoop.js
@@ -5,6 +5,8 @@ function() {
     this.frame = this.frame.bind(this);
     this.lastTime = 0;
     this.callback = function() {};
+    this.fps = 30;
+    this.interval = 1000/this.fps;
   }
 
   GameLoop.prototype.start = function(callback) {
@@ -12,12 +14,16 @@ function() {
     requestAnimationFrame(this.frame);
   };
 
+
   GameLoop.prototype.frame = function(time) {
-    var seconds = (time - this.lastTime) / 1000;
-    this.lastTime = time;
-    if (seconds < 0.2) {
-      this.callback(seconds);
+    var dt = time - this.lastTime;
+    if (dt >= this.interval) { // max fps
+      if (dt < 200) {
+        this.callback(dt/1000);
+      }
+      this.lastTime = time - dt % this.interval;
     }
+
     requestAnimationFrame(this.frame);
   };
 
